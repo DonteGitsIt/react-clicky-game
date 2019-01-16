6f336d0e5c668905dc157e3a2e5388aa1a11a096
DonteGitsIt <donte.ojeda@gmail.com>
2019-01-16 01:55:20 -0500

    Updates

---
+++ precache-manifest.d3a7fe57d2fa72213e8bf7904ed182d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "42b6138ceb9105e855e4",
+    "url": "/react-clicky-game/static/js/main.42b6138c.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/react-clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "42b6138ceb9105e855e4",
+    "url": "/react-clicky-game/static/css/main.6122df0e.chunk.css"
+  },
+  {
+    "revision": "cce62bbb237c446eefa15c06453ee80a",
+    "url": "/react-clicky-game/index.html"
+  }
+];
